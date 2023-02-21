3f87eb36382ccad655101c29569f9912dcf8f81f
Anthony Sogomo <tylorsoggy24@gmail.com>
2023-02-21 22:01:48 +0300

    Connected to database (mysql) and applied all migrations to django_poll.

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_poll",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
     }
 }
 
@@ -99,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "EAT"
 
 USE_I18N = True
 
